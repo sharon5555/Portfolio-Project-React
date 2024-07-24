4ac0ee8728b714829a08100352c5cf8316067294
Naza <sharon.doris54@gmail.com>
2024-07-24 15:28:28 +0100

    In the App file, i imported every other files into it

--- react-portfolio-website-0/src/App.jsx
+++ react-portfolio-website-0/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import Header from './components/Header/header'
+import Nav from './components/Nav/nav'
+import About from './components/About/about'
+import Experience from './components/Experience/experience'
+import Services from './components/Services/service'
+import Portfolio from './components/Portfolio/portfolio'
+import Testimonials from './components/Testimonials/testimonials'
+import Contact from './components/Contact/contact'
+import Footer from './components/Footer/footer'
 
 const App = () => {
 return (
-    <div>App</div>
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
+    </>
 );
 }
 
