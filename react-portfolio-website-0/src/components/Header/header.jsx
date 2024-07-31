0509a28c6d3e154323687d639547710e896c698e
Naza <sharon.doris54@gmail.com>
2024-07-31 18:38:49 +0100

    Added a CTA file, uploaded a CV and also updated the header jsx file

--- react-portfolio-website-0/src/components/Header/header.jsx
+++ react-portfolio-website-0/src/components/Header/header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const header = () => {
 return (
-    <div>header</div>
+    <header>
+        <div className= "container header_container">
+            <h5>Hello I'm</h5>
+            <h1>Doris Okeke</h1>
+            <h5 className="text-light">Frontend Developer</h5>
+            <CTA />
+        </div>
+    </header>
 )
 }
 
