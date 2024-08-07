778a9c8d2c93db06f2769f6a300cef5f27c496da
Naza <sharon.doris54@gmail.com>
2024-08-07 21:16:58 +0100

    Added the media queries in the header css file

--- react-portfolio-website-0/src/components/Nav/nav.jsx
+++ react-portfolio-website-0/src/components/Nav/nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import { FaHome } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa";
+import { FaBook } from "react-icons/fa6";
+import { RiServiceLine } from "react-icons/ri";
+import { MdOutlineContacts } from "react-icons/md";
 
 const nav = () => {
 return (
-    <div>nav</div>
+    <nav>
+        <a href="#"><FaHome /></a>
+        <a href="#about"><FaRegUser /></a>
+        <a href="#experience"><FaBook /></a>
+        <a href="#services"><RiServiceLine /></a>
+        <a href="#contact"><MdOutlineContacts /></a>
+    </nav>
 )
 }
 
