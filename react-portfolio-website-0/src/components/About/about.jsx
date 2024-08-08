a610a2274e93a925385ae72648135c1122cffae0
Naza <sharon.doris54@gmail.com>
2024-08-08 04:05:29 +0100

    updated about jsx file

--- react-portfolio-website-0/src/components/About/about.jsx
+++ react-portfolio-website-0/src/components/About/about.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './about.css'
 import Love from '../../assets/love.png'
+import { FiAward } from "react-icons/fi";
+import { FaUsers } from "react-icons/fa";
+import { FaFolder } from "react-icons/fa";
 
 const About = () => {
 return (
@@ -18,9 +21,30 @@ return (
             <div className="about_content">
                 <div className="about_cards">
                     <article className="about_card">
-                        
+                    <FiAward  className="about_icon"/>
+                        <h5>Experience</h5>
+                        <small>2 Years working</small>
+                    </article>
+
+                    <article className="about_card">
+                    < FaUsers className="about_icon"/>
+                        <h5>Clients</h5>
+                        <small>100+ Worldwide</small>
+                    </article>
+
+                    <article className="about_card">
+                    <FaFolder className="about_icon"/>
+                        <h5>Projects</h5>
+                        <small>10+ completed</small>
                     </article>
                 </div>
+
+                <p>
+                A frontend developer builds the user-facing parts of websites, using HTML, CSS, JavaScript, and frameworks like React for design and functionality.
+                </p>
+
+                <a href="#contact" className="btn btn-primary">Let's Talk</a>
+
             </div>
         </div>
     </section>
