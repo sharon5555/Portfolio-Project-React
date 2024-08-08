41b7cb2043b6b54d1f88e0cbebc4138e0c33eff9
Naza <sharon.doris54@gmail.com>
2024-08-08 02:46:12 +0100

    updated nav jsx file and also imported the useState

--- react-portfolio-website-0/src/components/Nav/nav.jsx
+++ react-portfolio-website-0/src/components/Nav/nav.jsx
@@ -5,17 +5,19 @@ import { FaRegUser } from "react-icons/fa";
 import { FaBook } from "react-icons/fa6";
 import { RiServiceLine } from "react-icons/ri";
 import { MdOutlineContacts } from "react-icons/md";
+import { useState } from 'react'
 
-const nav = () => {
+const Nav = () => {
+    const [activeNav, setActiveNav] = useState('#');
 return (
     <nav>
-        <a href="#" className='active'><FaHome /></a>
-        <a href="#about"><FaRegUser /></a>
-        <a href="#experience"><FaBook /></a>
-        <a href="#services"><RiServiceLine /></a>
-        <a href="#contact"><MdOutlineContacts /></a>
+        <a href="#" onClick={() => setActiveNav('#about')} className={activeNav === '#' ? 'active' : ''}><FaHome /></a>
+        <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaRegUser /></a>
+        <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaBook /></a>
+        <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
+        <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdOutlineContacts /></a>
     </nav>
 )
 }
 
-export default nav
+export default Nav;
