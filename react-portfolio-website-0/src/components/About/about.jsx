9901ee971e9c9fa404a892997c959eb5ff7ceec3
Naza <sharon.doris54@gmail.com>
2024-08-08 03:08:33 +0100

    In the about jsx file, added an image and also added card for the about informations

--- react-portfolio-website-0/src/components/About/about.jsx
+++ react-portfolio-website-0/src/components/About/about.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './about.css'
+import Love from '../../assets/love.png'
 
 const About = () => {
 return (
-    <section id='about'>About</section>
+    <section id='about'>
+        <h5>Get to Know</h5>
+        <h2>About Me</h2>
+
+        <div className="container about_container">
+            <div className="about_me">
+                <div className="about_me-image">
+                    <img src={Love} alt="About Image" />
+                </div>
+            </div>
+
+            <div className="about_content">
+                <div className="about_cards">
+                    <article className="about_card">
+                        
+                    </article>
+                </div>
+            </div>
+        </div>
+    </section>
 )
 }
 
