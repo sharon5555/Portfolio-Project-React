ff7ab96ef52dafca513d2e73df46d03ba6db23a5
Naza <sharon.doris54@gmail.com>
2024-08-12 22:27:27 +0100

    updated the experience jsx file

--- react-portfolio-website-0/src/components/Experience/experience.jsx
+++ react-portfolio-website-0/src/components/Experience/experience.jsx
@@ -1,9 +1,90 @@
 import React from 'react'
 import './experience.css'
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const experience = () => {
 return (
-    <section id='experience'>experience</section>
+    <section id='experience'>
+        <h5>What Skills I Have</h5>
+        <h2>My Experience</h2>
+
+        <div className="container experience_container">
+            <div className="experience_frontend">
+                <h3>Frontend Development</h3>
+                <div className="experience_content">
+                    <article className="experience_details">
+                    <BsPatchCheckFill />
+                    <h4>HTML</h4>
+                    <small className="text-light">Experienced</small>
+                    </article>
+
+                    <article className="experience_details">
+                    <BsPatchCheckFill />
+                    <h4>CSS</h4>
+                    <small className="text-light">Experienced</small>
+                    </article>
+
+                    <article className="experience_details">
+                    <BsPatchCheckFill />
+                    <h4>JavaScript</h4>
+                    <small className="text-light">Experienced</small>
+                    </article>
+
+                    <article className="experience_details">
+                    <BsPatchCheckFill />
+                    <h4>Bootstrap</h4>
+                    <small className="text-light">Experienced</small>
+                    </article>
+
+                    <article className="experience_details">
+                    <BsPatchCheckFill />
+                    <h4>Tailwind</h4>
+                    <small className="text-light">Experienced</small>
+                    </article>
+
+                    <article className="experience_details">
+                    <BsPatchCheckFill />
+                    <h4>React</h4>
+                    <small className="text-light">Experienced</small>
+                    </article>
+                </div>
+            </div>
+            <div className="experience_backend">
+            <h3>Frontend Development</h3>
+                <div className="experience_content">
+                    <article className="experience_details">
+                    <BsPatchCheckFill />
+                    <h4>Figma</h4>
+                    <small className="text-light">Experienced</small>
+                    </article>
+
+                    <article className="experience_details">
+                    <BsPatchCheckFill />
+                    <h4>Flutter</h4>
+                    <small className="text-light">Experienced</small>
+                    </article>
+
+                    <article className="experience_details">
+                    <BsPatchCheckFill />
+                    <h4>JavaScript</h4>
+                    <small className="text-light">Experienced</small>
+                    </article>
+
+                    <article className="experience_details">
+                    <BsPatchCheckFill />
+                    <h4>Bootstrap</h4>
+                    <small className="text-light">Experienced</small>
+                    </article>
+
+                    <article className="experience_details">
+                    <BsPatchCheckFill />
+                    <h4>Tailwind</h4>
+                    <small className="text-light">Experienced</small>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </section>
 )
 }
 
