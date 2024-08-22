cd9f68d01d16183c4d33452a8b9f1bc3a8e89a4b
Naza <sharon.doris54@gmail.com>
2024-08-23 00:41:01 +0100

    Update contact jsx file

--- react-portfolio-website-0/src/components/Contact/contact.jsx
+++ react-portfolio-website-0/src/components/Contact/contact.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import './contact.css'
+import { TfiEmail } from "react-icons/tfi";
 
 const contact = () => {
 return (
-    <section id='contact'>contact</section>
+    <section id='contact'>
+        <h5>Get In Touch</h5>
+        <h2>Contact Me</h2>
+
+        <div className='container contact_container'>
+            <div className="contact_options">
+                <article className='contact_option'>
+                    <TfiEmail />
+                    <h4>Email</h4>
+                    <h5>sharon.doris54@gmail.com</h5>
+                    <a href="mailto:sharon.doris54@gmail.com">Send a message</a>
+                </article>
+
+                <article className='contact_option'>
+                    <TfiEmail />
+                    <h4>Messenger</h4>
+                    <h5>Cynn</h5>
+                    <a href="my facebook url">Send a message</a>
+                </article>
+
+                <article className='contact_option'>
+                    <TfiEmail />
+                    <h4>Email</h4>
+                    <h5>sharon.doris54@gmail.com</h5>
+                    <a href="mailto:sharon.doris54@gmail.com">Send a message</a>
+                </article>
+            </div>
+            {/* END OF CONTACT OPTION */}
+
+
+        </div>
+    </section>
 )
 }
 
